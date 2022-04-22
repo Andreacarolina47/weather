680cc0238ffcaa4cdda4043407b267618fdc86b9
Andreacarolina47 <93480002+Andreacarolina47@users.noreply.github.com>
2022-04-22 13:49:03 +0800

    Added search bar

--- src/App.js
+++ src/App.js
@@ -2,26 +2,31 @@ import './App.css';
 import React, {useState} from "react";
 import axios from "axios";
 
-function App() {
-const [temperature, setTemperature]= useState(null); 
-const [received, setReceived]= useState(false); 
+export default function App(){
+  const [city, setCity]= useState(null);
+  const [received, setReceived]= useState(false);
 
-function showTemperature(response){
-setReceived(true);
-setTemperature(Math.round(response.data.main.temp));
-}
-
-  let url= `https://api.openweathermap.org/data/2.5/weather?q=lisbon&appid=02cf53f923b1744f0dbdf803cfd893b1&units=metric`
-  axios.get(url).then(showTemperature)
-  if (received){
-  return (
-    <div className="App">
-      <h1>Hello App</h1>
-      <h2>The temperature in Lisbon is {temperature}°C</h2>
-    </div>
-  );} else{
-    return(<h1>Hello App</h1>)
+  function getCity(event){
+    event.preventDefault();
+    setCity(event.target.value);
   }
-}
 
-export default App;
+  function handleSearch(event){
+    event.preventDefault();
+  }
+
+  let form = <form onSubmit={handleSearch}>
+    <input type="search" placeholder='Enter the name of a place' onChange={getCity} />
+    <input type="submit" value="Search"  />
+  </form>
+
+  if(received){
+    return(
+      <div>{form}
+      </div>
+    );} else{
+      return(
+        <div>{form}</div>
+      )
+    }
+  }
