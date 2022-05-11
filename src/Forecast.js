396c055c248cc7ef39850ed15b8f7d69ad71c58f
Andreacarolina47 <93480002+Andreacarolina47@users.noreply.github.com>
2022-05-11 22:33:30 +0800

    Added forecast information for one day

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,28 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
 import './App.css';
+import ForecastDay from "./ForecastDay";
 
 export default function Forecast(props){
+    const [loaded, setLoaded]= useState(false);
+    const [forecastData, setForecastData]= useState(null);
+    const [icon, setIcon]= useState(null);
 
     function searchForecast(response){
-        
+        setLoaded(true);
+        setForecastData(response.data.daily)
+        setIcon(`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)      
     }
 
+if (loaded){
+    return(
+        <div className="row">
+            <div className="col forecastContainer" >
+                <ForecastDay data={forecastData[0]} icon={icon}/>
+            </div>
+        </div>
+    )
+} else{   
 let apiKey = "02cf53f923b1744f0dbdf803cfd893b1";
 let latitude= props.coordinates.lat;
 let longitude= props.coordinates.lon;
 let url= `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
 axios.get(url).then(searchForecast);
-    return(
-        <div className="row">
-            <div className="col forecastContainer" >
-                <div className="forecast">Thu</div>
-                <div className="forecast"><img src="http://openweathermap.org/img/wn/01d@2x.png" alt="icon"/></div>
-                <div className="forecast">
-                    <span className="forecastTemp">27°</span>
-                    <span className="forecastTemp">25°</span>
-                </div>
-            </div>
-        </div>
-    )
+
+return null }
+    
 }
---
+++ src/ForecastDay.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function ForecastDay(props){
+    function maxTemp(){
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`}
+
+    function minTemp(){
+        let temperature = Math.round(props.data.temp.min);
+        return `${temperature}°`}
+
+    function day(){
+    let date= new Date(props.data.dt*1000);
+    let day= date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+    return days[day];
+    }
+
+    return (
+        <div>
+            <div className="forecast">{day()}</div>
+                <div className="forecast">{props.icon}</div>
+                <div className="forecast">
+                    <span className="forecastTemp">{maxTemp()}</span>
+                    <span className="forecastTemp">{minTemp()}</span>
+                </div>
+        </div>
+    )
+}
